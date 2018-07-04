26cea52b78adebc2466e937a6a3c30639ba41998
margo <margo.sockolova2010@yandex.ru>
2018-07-04 19:39:31 +0300

    #2 just stupid commit for home working

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -27,6 +27,16 @@ module.exports = {
                     { loader: 'css-loader' },
                 ],
             },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    },
+                }],
+            },
         ],
     },
     devServer: {
